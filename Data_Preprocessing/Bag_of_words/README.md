d3f128850b3abff976d3d433b54d5b66a94ff081
Yükselcan Sevil <44815724+Yuucas@users.noreply.github.com>
2024-08-06 15:28:22 +0300

    Add files via upload

---
+++ Data_Preprocessing/Bag_of_words/README.md
@@ -0,0 +1,15 @@
+# Bag of Words (BoW)
+Bag of Words is a simple yet effective technique for converting text data into numerical representations that machine learning algorithms can understand.
+
+<p align="center">
+  <img width="600" src="https://github.com/Yuucas/NLP/blob/9ca7b4af4c31a97a303108b9e2b882efe3003a9b/Bag_of_words/table.png"></a>
+</p>
+<div align="center">
+
+
+<div align="left">
+  
+### How it works:
+* **Tokenization:** The text is divided into individual words (tokens).
+* **Vocabulary Creation:** A unique list of all words in the text corpus is created. This is called the vocabulary.
+* **Vectorization:** Each document is represented as a numerical vector, where each element corresponds to a word in the vocabulary. The element's value represents the frequency of that word in the document.
---
+++ Data_Preprocessing/Bag_of_words/bag_of_words.py
@@ -0,0 +1,43 @@
+# Import Libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+
+if __name__ == "__main__":
+    
+    # Read the IMDB Dataset 
+    # https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews?resource=download
+
+    dataframe = pd.read_csv("NLP_Tutorials\IMDB Dataset.csv")
+
+    print("Shape of the data: ", dataframe.shape)
+
+    print("Head of the data: ", dataframe.head())
+
+    # Create a new column "Category" which represent 1 if the sentiment is positive or 0 if it is negative
+    dataframe['Category'] = dataframe['sentiment'].apply(lambda x: 1 if x =='positive' else 0)
+
+    # Check the distribution
+    print(dataframe["Category"].value_counts())
+
+    # Split the data into train and test
+    X_train, X_test, y_train, y_test = train_test_split(dataframe["review"], dataframe["Category"], test_size=0.2)
+
+    # Create pipeline
+    clf = Pipeline([
+    ('vectorizer', CountVectorizer()),
+    ('randomforest', RandomForestClassifier())
+    ])
+
+    # Start the training
+    clf.fit(X_train, y_train)
+
+    # Test the classifier
+    y_pred = clf.predict(X_test)
+
+    # Print the result
+    print(classification_report(y_test, y_pred))
